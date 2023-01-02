f7c2f42314fcb4b999131d55e1f7d4863d9a73d4
rmarpaung <rimbun@live.com>
2023-01-01 22:36:27 -0500

    temperature conversion

--- Exercise/03_Conditional/06-2.py
+++ Exercise/03_Conditional/06-2.py
@@ -5,62 +5,53 @@ target = input("Masukkan satuan konversi: ")
 if origin == "celcius":
     if target == "celcius":
         result = value * 1
-        print("Hasil konversi %s %s adalah %s %s" % (value, origin, result, target))
     elif target == "reamur":
         result = value * 0.8
-        print("Hasil konversi %s %s adalah %s %s" % (value, origin, result, target))
     elif target == "fahrenheit":
         result = value * 1.8 + 32
-        print("Hasil konversi %s %s adalah %s %s" % (value, origin, result, target))
     elif target == "kelvin":
         result = value + 273.15
-        print("Hasil konversi %s %s adalah %s %s" % (value, origin, result, target))
     else:
         print("Satuan konversi tidak diketahui")
+        quit()
 elif origin == "reamur":
     if target == "celcius":
         result = value / 0.8
-        print("Hasil konversi %s %s adalah %s %s" % (value, origin, result, target))
     elif target == "reamur":
         result = value * 1
-        print("Hasil konversi %s %s adalah %s %s" % (value, origin, result, target))
     elif target == "fahrenheit":
         result = value * 2.25 + 32
-        print("Hasil konversi %s %s adalah %s %s" % (value, origin, result, target))
     elif target == "kelvin":
         result = value / 0.8 + 273.15
-        print("Hasil konversi %s %s adalah %s %s" % (value, origin, result, target))
     else:
         print("Satuan konversi tidak diketahui")
+        quit()
 elif origin == "fahrenheit":
     if target == "celcius":
         result = (value - 32)/1.8
-        print("Hasil konversi %s %s adalah %s %s" % (value, origin, result, target))
     elif target == "reamur":
         result = (value - 32) / 2.25
-        print("Hasil konversi %s %s adalah %s %s" % (value, origin, result, target))
     elif target == "fahrenheit":
         result = value * 1
-        print("Hasil konversi %s %s adalah %s %s" % (value, origin, result, target))
     elif target == "kelvin":
         result = (value + 459.67) / 1.8
-        print("Hasil konversi %s %s adalah %.3f %s" % (value, origin, result, target))
     else:
         print("Satuan konversi tidak diketahui")
+        quit()
 elif origin == "kelvin":
     if target == "celcius":
         result = value - 273.15
-        print("Hasil konversi %s %s adalah %s %s" % (value, origin, result, target))
     elif target == "reamur":
         result = (value - 273.15) * 0.8
-        print("Hasil konversi %s %s adalah %s %s" % (value, origin, result, target))
     elif target == "fahrenheit":
         result = value * 1.8 - 459.67
-        print("Hasil konversi %s %s adalah %s %s" % (value, origin, result, target))
     elif target == "kelvin":
         result = value * 1
-        print("Hasil konversi %s %s adalah %s %s" % (value, origin, result, target))
     else:
         print("Satuan konversi tidak diketahui")
+        quit()
 else:
     print("Satuan asal tidak diketahui")
+    exit()
+
+print("Hasil konversi %s %s adalah %s %s" % (value, origin, result, target))
